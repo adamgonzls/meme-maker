d94782d8245f4279b3266e218a5c34bebaae81b1
Adam Gonzales <adamgonzales1@gmail.com>
2022-07-01 10:27:09 -0700

    feature: add random image and url to dom. - set image css to max 100% - remove semicolons using prettierrc

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,25 +1,37 @@
-import memesData from "../memesData";
+import { useState } from "react"
+import memesData from "../memesData"
 
 export default function MemeForm() {
+  const [memeImage, setMemeImage] = useState("https://i.imgflip.com/tau4.jpg")
+  const [memeImageAlt, setMemeImageAlt] = useState("I Should Buy A Boat Cat")
+
   function handleGetMemeImage(e) {
-    const { memes } = memesData.data;
-    const selectedImageURL =
-      memes[Math.floor(Math.random() * memes.length)].url;
-    console.log(selectedImageURL);
+    const { memes } = memesData.data
+    const randomMeme = memes[Math.floor(Math.random() * memes.length)]
+    const randomMemeImageURL = randomMeme.url
+    const randomMemeAlt = randomMeme.name
+    setMemeImage(randomMemeImageURL)
+    setMemeImageAlt(randomMemeAlt)
   }
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Top text"
-        className="something"
+    <main>
+      <div>
+        <input
+          type="text"
+          placeholder="Top text"
+          className="something"
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+        />
+        <button onClick={handleGetMemeImage}>Get a new meme image 🖼</button>
+      </div>
+      <img
+        src={memeImage}
+        alt={memeImageAlt}
       />
-      <input
-        type="text"
-        placeholder="Bottom text"
-      />
-      <button onClick={handleGetMemeImage}>Get a new meme image 🖼</button>
-    </div>
-  );
+    </main>
+  )
 }
